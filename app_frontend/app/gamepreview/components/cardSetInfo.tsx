a577814ff0300e3dfc214fbbba41def00338a9ee
Eric Asante <eric@Erics-MacBook-Pro.local>
2026-03-20 14:04:58 -0400

    UI Change

--- app_frontend/app/gamepreview/components/cardSetInfo.tsx
+++ app_frontend/app/gamepreview/components/cardSetInfo.tsx
@@ -19,21 +19,21 @@ export default function CardSetInfo() {
 
         :
         
-        <div className="flex flex-col   row-span-1 col-span-1 rounded-2xl p-4 space-y-2">
+        <div className="flex flex-col row-span-1 col-span-1 rounded-2xl p-4 space-y-2 min-h-0 ">
           <div className="bg-red-400 aspect-4/3 h-1/3 ">
           </div>
 
           <div className="aspect-4/3 h-32 ">
-            <h2 className="font-bold text-text text-3xl w-full overflow-hidden line-clamp-3">
+            <h2 className="font-bold text-text text-2xl w-full overflow-hidden line-clamp-3">
               {text}
             </h2>
           </div>
     
-          <div className="flex flex-row">
-            <h1 className=" text-text text-md w-full overflow-hidden line-clamp-18">
+          <div className="flex flex-row h-full">
+            <h1 className=" text-text text-sm w-full overflow-hidden line-clamp-15">
               {text}
             </h1>
-            <h1 className="font-semibold text-text text-md w-full overflow-hidden line-clamp-5">
+            <h1 className=" text-text text-sm w-full overflow-hidden line-clamp-15">
               {text}
             </h1>
 
--- app_frontend/app/gamepreview/components/loadingCardSetItem.tsx
+++ app_frontend/app/gamepreview/components/loadingCardSetItem.tsx
@@ -3,14 +3,14 @@ import { SkeletonTheme } from "react-loading-skeleton";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-export default function LoadingCardSetItem() {
+export default function LoadingCardSetItem({className}: {className: string}) {
   return (
     <SkeletonTheme
       baseColor="var(--foreground)"
       highlightColor="var(--highlight)"
     >
 
-        <div className="aspect-square w-[15%] flex-shrink-0">
+        <div className={`aspect-square shrink-0 ${className}`}>
             <Skeleton className="w-full h-full rounded-xl" />
         </div>
 
